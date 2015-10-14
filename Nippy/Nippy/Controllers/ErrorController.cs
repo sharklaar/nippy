0c03573d9c3f9e455c3ea08c2f17bfd8613c5cca
Marc Daffern <marc.daffern@bglgroup.co.uk>
2015-10-14 12:36:02 +0100

    added project

---
+++ Nippy/Nippy/Controllers/ErrorController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace Nippy.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Http404()
+        {
+            Response.StatusCode = 404;
+
+            return View();
+        }
+    }
+}
---
+++ Nippy/Nippy/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Nippy.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
