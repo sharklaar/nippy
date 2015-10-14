1f3d4a1a873045822426e6354fd58416f3045732
Marc Daffern <marc.daffern@bglgroup.co.uk>
2015-10-14 12:55:20 +0100

    added camera support

--- Nippy/Nippy/Scripts/_references.js
+++ Nippy/Nippy/Scripts/_references.js
@@ -5,3 +5,4 @@
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.min.js" />
 /// <reference path="jquery-ui.min-1.11.1.js" />
+/// <reference path="../content/scripts/capture.js" />
